efa82b6dda51f4d6d37fcf23b9d495db76cd3521
César Bernardini <cbernardini@barracuda.com>
2018-12-19 12:44:11 +0100

    testing the github mode

--- js/config.js
+++ js/config.js
@@ -6,14 +6,14 @@ var config = {
 
   // Mode 'GITHUB' for Github Pages, 'SERVER' for Self Hosted
   // Defaults to Server mode if not specified
-  mode: 'SERVER',
+  mode: 'GITHUB',
 
   // If Github mode is set, your Github username, repo name, 
   // and branch to get files from.
   github: {
-    username: 'yourusername',
-    repo: 'yourrepo',
-    branch: 'gh-pages',
+    username: 'mesarpe',
+    repo: 'mesarpe.github.io',
+    branch: 'master',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
     // prefix: 'subdirectory',
